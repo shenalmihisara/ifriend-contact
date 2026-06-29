f780b1f5b1f2a1b03bca1f547396166a67db36ef
shenalmihisara <shenalmihisara51@gmail.com>
2026-06-30 01:43:40 +0530

    Enhance contact addition process with confirmation and option to add more contacts

--- ifriend.java
+++ ifriend.java
@@ -5,25 +5,45 @@ class ifriend{
 		System.out.println("+-----------------------------------+");
 		System.out.println("|      Add Contact to the list      |");
 		System.out.println("+-----------------------------------+");
+
+		int i = 1;
 		
-		System.out.println("index");
-		System.out.println("=====");
-		System.out.println("");
+		while(true){
+				String id = String.format("C%04d", i);
+				System.out.println(id);
 		
-		System.out.print("Name                : ");
-		String Name = input.next();
+				System.out.println("=====");
+				System.out.println("");
+			
+				System.out.print("Name                : ");
+				String Name = input.next();
 		
-		System.out.print("Phone Number      : ");
-		int phone_number=input.nextInt();
+				System.out.print("Phone Number      : ");
+				int phone_number=input.nextInt();
 		
-		System.out.print("Company Name      : ");
-		String Company=input.next();
+				System.out.print("Company Name      : ");
+				String Company=input.next();
 		
-		System.out.print("Salary            : ");
-		int Salary=input.nextInt();
+				System.out.print("Salary            : ");
+				int Salary=input.nextInt();
 		
-		System.out.print("B'Day(YYYY-MM_DD) : ");
-		String BDay=input.next();
+				System.out.print("B'Day(YYYY-MM_DD) : ");
+				String BDay=input.next();
+				System.out.println("");
+			
+			System.out.println("Contact has been added succesfully...");
+			System.out.println("");
+			System.out.print("Do you want add another contact(Y/N) : ");
+			String another_num = input.next();
+			
+			i++;//next id
+		
+			if(another_num.equalsIgnoreCase("Y")){
+				System.out.println(" ");
+			}else{
+				break;
+			}
+		}		
 	}
 }
 
