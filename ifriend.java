2aba9a85c58c1490e37cc13511775001de119409
shenalmihisara <shenalmihisara51@gmail.com>
2026-06-30 13:10:43 +0530

    Improve contact addition flow with validation and option to add more contacts

--- ifriend.java
+++ ifriend.java
@@ -1,15 +1,16 @@
 import java.util.*;
 class ifriend{
 	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
-		System.out.println("+-----------------------------------+");
-		System.out.println("|      Add Contact to the list      |");
-		System.out.println("+-----------------------------------+");
 
 		int i = 1;
-		
-		while(true){
-				String id = String.format("C%04d", i);
+
+		while(true){			
+			Scanner input = new Scanner(System.in);
+			System.out.println("+-----------------------------------+");
+			System.out.println("|      Add Contact to the list      |");
+			System.out.println("+-----------------------------------+");
+
+				String id = String.format("C%04d", i);		
 				System.out.println(id);
 		
 				System.out.println("=====");
@@ -19,8 +20,22 @@ class ifriend{
 				String Name = input.next();
 		
 				System.out.print("Phone Number      : ");
-				int phone_number=input.nextInt();
-		
+				String phone_number=input.next();
+				
+				if (phone_number.matches("\\d{10}")) {
+				}else{
+					System.out.println("");
+					System.out.println("Inavalid number...");
+					System.out.println("");					
+					System.out.print("Do you want add another contact(Y/N) : ");
+					String another_num = input.next();
+					
+					if(another_num.equalsIgnoreCase("Y")){
+						continue;
+					}else{
+						break;
+					}
+				}		
 				System.out.print("Company Name      : ");
 				String Company=input.next();
 		
@@ -40,6 +55,7 @@ class ifriend{
 		
 			if(another_num.equalsIgnoreCase("Y")){
 				System.out.println(" ");
+				System.out.println("=====================================");
 			}else{
 				break;
 			}
